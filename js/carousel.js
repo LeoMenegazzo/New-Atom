07caa9784bcecda8eecc060e2398f3fa5fdebcaf
Leonardo Ari Santos Menegazzo <leonardoarimenegazzo@gmail.com>
2022-03-11 13:34:51 -0400

    6

--- js/carousel.js
+++ js/carousel.js
@@ -54,3 +54,59 @@ $('.wq-case-carousel').slick({
         }
       ]
   });
+
+  $('.depoimentos-carousel').slick({
+    dots: true,
+    infinite: true,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+});
+
+$('.wq-carousel-depoimentos').slick({
+    slidesToShow: 2,
+    arrows: false,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+    $('.wq-carousel-site').slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        speed: 500,
+        slidesToShow: 1,
+        fade: true,
+        cssEase: 'linear',
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+            }
+          }
+        ]
+    });
+
+    
